799ee6fed19357e84fce2a70a7d58027c7ae5081
Gabe Rogan <rogangabe@gmail.com>
2023-05-01 14:29:25 -0400

    add mentions

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
+const { differenceInWeeks } = require('date-fns');
+
+
+// Just a way to count weekdays
+const now = new Date();
+const weeksSinceEpoch = differenceInWeeks(now, new Date('2023-01-01'), /* Sunday */ { roundingMethod: 'floor' });
+const dayOfWeek = now.getDay() - 1;
+const weekdaysSinceEpoch = weeksSinceEpoch * 5 + dayOfWeek;
+
+// Members, ids required for mentions
+// Find user ids like this: https://www.workast.com/help/article/how-to-find-a-slack-user-id/
+const Tommy = '<@U03GCNZKG3U>'
+const Gabe = '<@U03FL4LNCRW>'
+const Benji = '<@U03B81S4QEN>'
+const Joshua = '<@UJNLRPGMS>'
+const Jeferson = '<@U03HWKKQ1MX>'
+const CJ = '<@U03DBHKAK9T>'
+const Brett = '<@UT4SH1ZNX>'
+const Burhan = '<@U042ENP5AKT>'
+const Ahsan = '<@U04C4F8QBFE>'
+const Artem = '<@U03740LFH6D>'
+const Christian = '<@U03G9RULWJH>'
+const Ken = '<@U0185215CGL>'
+// const Tim = '<@U031MFG63FA>' - not in rotation
+
+// See skills sheet to make rotations https://docs.google.com/spreadsheets/d/1RpI3L4dEzy9XLHY1Zi1o7MddlepGvM4XAg0H45iZ6iI/edit#gid=0
+const rotation1 = [Tommy, Gabe, Benji, Joshua];
+const rotation2 = [Jeferson, Burhan, Christian, Brett];
+const rotation3 = [CJ, Ken, Ahsan, Artem];
+
+// We can change this but the script is currently designed same length rotations
+if (rotation1.length !== rotation2.length || rotation1.length !== rotation3.length) {
+  throw new Error('Rotations should have the same length');
+}
+
+const getReviewersByIndex = (index) => {
+  return [rotation1[index], rotation2[index], rotation3[index]];
+}
+
+// Pick reviewers
+const todayReviewers = getReviewersByIndex(weekdaysSinceEpoch % rotation1.length);
+const tomorrowReviewers = getReviewersByIndex((weekdaysSinceEpoch + 3) % rotation1.length);
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -15,8 +57,9 @@ const app = new App({
   await app.start();
 
   await app.client.chat.postMessage({
-    channel: '#gabe-test',
-    text: 'hello',
+    channel: '#clientz-internal',
+    text: '@Gabe Rogan',
+    text: `Today's reviewers: ${todayReviewers.join(', ')}.\nTomorrow's reviewers: ${tomorrowReviewers.join(', ')}.`,
   })
 
   await app.stop();
