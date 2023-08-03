7e1457202b62695008ed114bf2189e2a573114a9
Gabe Rogan <rogangabe@gmail.com>
2023-08-03 10:29:11 +0200

    update rotation, use groups

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
-const { differenceInWeeks } = require('date-fns');
+require('dotenv').config()
+const { App } = require('@slack/bolt')
+const { differenceInWeeks } = require('date-fns')
 
 // Members, ids required for mentions
 // Find user ids like this: https://www.workast.com/help/article/how-to-find-a-slack-user-id/
 const Tommy = '<@U03GCNZKG3U>'
 const Gabe = '<@U03FL4LNCRW>'
-const Benji = '<@U03B81S4QEN>'
 const Joshua = '<@UJNLRPGMS>'
 const CJ = '<@U03DBHKAK9T>'
 const Brett = '<@UT4SH1ZNX>'
@@ -16,42 +15,50 @@ const Artem = '<@U03740LFH6D>'
 const Ken = '<@U0185215CGL>'
 const Jesse = '<@U052VKWU1S5>'
 const John = '<@U056RB01535>'
+const Denys = '<@U04QMGUR21L>'
+const Andrii = '<@U04REG49FMY>'
+const Roman = '<@U04PJ40BY4W>'
+const Vasyl = '<@U04PQJA5TGB>'
+const Oleksandr = '<@U04SJTYKT2P>'
+const Marko = '<@U04SK2D4GQJ>'
+const Joseph = '<@U05BX30V37S>'
+const Sam = '<@U05DLNWNBNV>'
 // const Tim = '<@U031MFG63FA>' - not in rotation
 
 // See skills sheet to make rotations https://docs.google.com/spreadsheets/d/1RpI3L4dEzy9XLHY1Zi1o7MddlepGvM4XAg0H45iZ6iI/edit#gid=0
-const rotation1 = [Tommy, Gabe, Benji, Joshua];
-const rotation2 = [Jesse, Ken, John, Artem];
-const rotation3 = [CJ, Burhan, Ahsan, Brett];
+const group1 = [Tommy, Jesse, CJ, Oleksandr, Andrii]
+const group2 = [Gabe, Sam, Ken, Burhan, Vasyl]
+const group3 = [John, Joseph, Ahsan, Roman, Marko]
+const group4 = [Joshua, Artem, Brett, Denys]
+
+const rotation = [group1, group2, group3, group4]
+
+// Just a way to count weekdays
+const getWeekdaysSinceEpoch = () => {
+  const now = new Date()
+  const weeksSinceEpoch = differenceInWeeks(now, new Date('2023-01-01'), /* Sunday */ { roundingMethod: 'floor' })
+  const dayOfWeek = now.getDay() - 1
+  return weeksSinceEpoch * 5 + dayOfWeek
+}
 
 exports.handler = async () => {
-  // We can change this but the script is currently designed same length rotations
-  if (rotation1.length !== rotation2.length || rotation1.length !== rotation3.length) {
-    throw new Error('Rotations should have the same length');
-  }
-
-  // Just a way to count weekdays
-  const now = new Date();
-  const weeksSinceEpoch = differenceInWeeks(now, new Date('2023-01-01'), /* Sunday */ { roundingMethod: 'floor' });
-  const dayOfWeek = now.getDay() - 1;
-  const weekdaysSinceEpoch = weeksSinceEpoch * 5 + dayOfWeek;
-
   // Pick reviewers
-  const getReviewersByIndex = (index) => [rotation1[index], rotation2[index], rotation3[index]];
-  const todayReviewers = getReviewersByIndex(weekdaysSinceEpoch % rotation1.length);
-  const tomorrowReviewers = getReviewersByIndex((weekdaysSinceEpoch + 1) % rotation1.length);
+  const weekdaysSinceEpoch = getWeekdaysSinceEpoch()
+  const todayReviewers = rotation[weekdaysSinceEpoch % rotation.length]
+  const tomorrowReviewers = rotation[(weekdaysSinceEpoch + 1) % rotation.length]
 
   // Start slack bolt app and send message
   const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
-  });
+  })
 
-  await app.start();
+  await app.start()
 
   await app.client.chat.postMessage({
     channel: '#clientz-internal',
     text: `Today's reviewers: ${todayReviewers.join(', ')}.\nTomorrow's reviewers: ${tomorrowReviewers.join(', ')}.`,
   })
 
-  await app.stop();
+  await app.stop()
 }
